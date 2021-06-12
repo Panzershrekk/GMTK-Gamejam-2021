739197ae39a253306def0293a0bf06c8d4a1bf0f
Panzershrekk <32634884+Panzershrekk@users.noreply.github.com>
2021-06-12 16:47:40 +0200

    Adding temporary UI adding more functionnalities

--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUIManager : MonoBehaviour
 {
-    
+    public GameManager GameManager;
+    public Image SharkLifeFill;
+    public Image BoatLifeFill;
+
     public void UpdateUI()
     {
-
+        Debug.Log("Wesh");
+        SharkLifeFill.fillAmount = GameManager.CharacterBehaviour.currentHealth / GameManager.CharacterBehaviour.maxHitpoint;
+        BoatLifeFill.fillAmount = GameManager.BoatBehaviour.currentHealth / GameManager.BoatBehaviour.maxHitpoint;
     }
 }
