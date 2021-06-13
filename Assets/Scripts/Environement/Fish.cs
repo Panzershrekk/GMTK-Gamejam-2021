9986866b6f3e865b722c9f484a5501b38a88fe97
Panzershrekk <32634884+Panzershrekk@users.noreply.github.com>
2021-06-13 17:23:34 +0200

    Sound

--- Assets/Scripts/Environement/Fish.cs
+++ Assets/Scripts/Environement/Fish.cs
@@ -7,10 +7,13 @@ public class Fish : MonoBehaviour
     public float HealthRestoreValue;
     public Transform baseParent;
     public Animator destroyFishAnimator;
+    public AudioSource audioSource;
+
 
     public void Trigger()
     {
         GetComponent<Collider2D>().enabled = false;
+        audioSource.Play();
         destroyFishAnimator.Play("FishDestroy");
         Destroy(baseParent.gameObject, 0.5f);
     }
